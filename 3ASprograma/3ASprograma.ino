04873db16844c853d229099ca573c660a1c6ca9e
Thomas Gomes <thomasbago@hotmail.com>
2021-05-23 19:20:15 -0300

    Adicionado projeto vazio arduino.

---
+++ 3ASprograma/3ASprograma.ino
@@ -0,0 +1,19 @@
+// alimentador de aves automatico solar 
+
+void setup() 
+{
+  // put your setup code here, to run once:
+
+}
+
+
+
+
+
+
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+
+}
